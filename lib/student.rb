de2591112f81dc8a3fceba9e9da7c4e4831ad4f2
Laura <lnkeat@gmail.com>
2021-12-21 13:16:34 -0700

    commit tree e4918529a6b2b54df676e06d24f6bf5e94dc113a

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+# require_relative "../lib/teacher"
+require_relative "../lib/user"
+
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string)
+    @string = string
+    self.knowledge << @string
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
